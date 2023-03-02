3b4b7362e69b8dc2a9376faed7e5e42e3f0a23c7
Mariia Akymenko <dafnarianna@gmail.com>
2023-03-03 01:09:56 +0200

    delete import

--- src/components/ContactElement/ContactElement.jsx
+++ src/components/ContactElement/ContactElement.jsx
@@ -2,7 +2,7 @@ import { EditContact } from 'components/EditContact/EditContact';
 import { Button } from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
-import { deleteContact, updateContact } from 'redux/contacts/operations';
+import { deleteContact } from 'redux/contacts/operations';
 import { BtnBlock, ElContact, Name, Number } from './ContactElement.styled';
 
 export const ContactElement = ({ id, name, number }) => {
