9bfa96cefb99af0bca8fe3f77fcb84e629307c5d
Mariia Akymenko <dafnarianna@gmail.com>
2023-03-03 14:52:04 +0200

    add loader to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,20 +3,25 @@ import { ContactsPage } from 'pages/ContactsPage/ContactsPage';
 import { Navigation } from 'components/Navigation/Navigation';
 import LogInPage from 'pages/LogInPage/LogInPage';
 import RegisterPage from 'pages/RegisterPage/RegisterPage';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/user/operations';
 import { PublicRoute } from './PublicRout';
 import { PrivateRoute } from './PrivetRoute';
+import { selectIsRefreshing } from 'redux/user/selectors';
+import { Loader } from './Loader/Loader';
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <div>
       <Routes>
         <Route path="/" element={<Navigation />}>
